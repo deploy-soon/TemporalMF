728ab30b0e0dcfdb7e98ac7e639446ab47b9e349
soon <dyd3312@gmail.com>
2020-02-19 16:51:53 +0900

    add bias to matrix factorization

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader,random_split
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class COOMatrix(Dataset):
 
@@ -44,9 +46,29 @@ class BaseMF(nn.Module):
         preds = (self.user_factor(user) * self.item_factor(item)).sum(1, keepdim=True)
         return preds.squeeze()
 
+
+class BaseBiasMF(nn.Module):
+
+    def __init__(self, users, items, factors):
+        super().__init__()
+        self.user_factor = nn.Embedding(users, factors)
+        self.item_factor = nn.Embedding(items, factors)
+        self.user_biases = nn.Embedding(users, 1)
+        self.item_biases = nn.Embedding(items, 1)
+
+    def forward(self, user, item):
+        user_bias = self.user_biases(user)
+        item_bias = self.item_biases(item)
+        user_factor = self.user_factor(user)
+        item_factor = self.item_factor(item)
+        preds = (user_factor * item_factor).sum(1, keepdim=True) +\
+            user_bias + item_bias
+        return preds.squeeze()
+
+
 class Train:
 
-    def __init__(self, factors, batch_size=16, train_ratio=0.8, epochs=10):
+    def __init__(self, factors, batch_size=128, train_ratio=0.8, epochs=10):
 
         self.factors = factors
 
@@ -61,7 +83,7 @@ class Train:
                                        shuffle=True)
         self.vali_loader = DataLoader(vali_set, batch_size=batch_size,
                                       shuffle=True)
-        self.model = BaseMF(self.data.users, self.data.items, self.factors)
+        self.model = BaseBiasMF(self.data.users, self.data.items, self.factors).to(device)
 
         self.epochs = epochs
 
@@ -72,37 +94,55 @@ class Train:
         pbar = tqdm(enumerate(self.train_loader), total=len(self.train_loader),
                     desc="({0:^3})".format(epoch))
         for batch, ((row, col), val) in pbar:
+            row = row.to(device)
+            col = col.to(device)
+            val = val.to(device)
             optimizer.zero_grad()
             preds = self.model(row, col)
             loss = loss_func(preds, val)
             loss.backward()
-
             optimizer.step()
             total_loss += loss.item()
-            batch_loss = loss.item() / len(row)
+            batch_loss = loss.item()
             pbar.set_postfix(train_loss=batch_loss)
-        total_loss /= self.train_num
+        total_loss /= (self.train_num)
         return total_loss[0]
 
     def validate(self, epoch, loss_func):
         self.model.eval()
         total_loss = torch.Tensor([0])
         for batch, ((row, col), val) in enumerate(self.vali_loader):
+            row = row.to(device)
+            col = col.to(device)
+            val = val.to(device)
             preds = self.model(row, col)
             loss = loss_func(preds, val)
             total_loss += loss.item()
-        total_loss /= self.vali_num
+        total_loss /= (self.vali_num)
         return total_loss[0]
 
+    def test(self):
+        self.model.eval()
+        for (row, col), val in self.vali_loader:
+            row = row.to(device)
+            col = col.to(device)
+            val = val.to(device)
+            preds = self.model(row, col)
+            for v, p in zip(val, preds):
+                print("actual: {}, predict: {}".format(v, p))
+            break
+
     def run(self):
-        loss_func = nn.MSELoss()
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=1e-6)
+        loss_func = nn.MSELoss(reduction="sum")
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
         for epoch in range(self.epochs):
             train_loss = self.train(epoch, loss_func, optimizer)
             vali_loss = self.validate(epoch, loss_func)
-            print("train loss: {} vali loss: {}".format(train_loss, vali_loss))
+            print("train loss: {:.4} vali loss: {:.4}".format(train_loss, vali_loss))
+        self.test()
+
 
 if __name__ == "__main__":
-    train = Train(factors=3)
+    train = Train(factors=10, epochs=100)
     train.run()
 
